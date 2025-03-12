a5cb22cfa7f70b1595f919ebbf9fb0bb32ef1a06
Tunez <163091834+Tunez666@users.noreply.github.com>
2025-03-12 22:27:35 +0800

    Delete threeadd directory

--- threeadd/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$Link=mysqli_connect('127.0.0.1', 'root', '', 'taxopark');
-if(!$Link){
-    echo 'Ошибка подключения к бд. Код ошибки: '. mysqli_connect_error(). 'ошибка: '. mysqli_connect_error();
-    exit;
-}
-?>
